c8f7307bbe4ddd28ffa8b784fab77b14b26835a9
jordy-manner <jordy.manner@milkcreation.fr>
2021-09-30 16:15:45 +0200

    feat/v1.0.5 remote Providers comment + move console + env in README

--- src/Providers/RoutingServiceProvider.php
+++ src/Providers/RoutingServiceProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Pollen\Container\BootableServiceProvider;
+use Pollen\Kernel\Container\BootableServiceProvider;
 use Pollen\Routing\RouterInterface;
 use Pollen\Support\Proxy\AssetProxy;
 
@@ -12,21 +12,15 @@ class RoutingServiceProvider extends BootableServiceProvider
 {
     use AssetProxy;
 
-    /**
-     * @inheritDoc
-     */
     public function boot(): void
     {
         /** @var RouterInterface $router */
-        $router = $this->getContainer()->get(RouterInterface::class);
+        $router = $this->app->get(RouterInterface::class);
 
-        $router->get(
-            '/',
-            function () {
-                $this->asset()->enqueueTitle('Welcome');
+        $router->get('/', function () {
+            $this->asset()->enqueueTitle('Welcome');
 
-                return view('index', ['name' => 'John Doe']);
-            }
-        );
+            return view('index', ['name' => 'John Doe']);
+        });
     }
 }
