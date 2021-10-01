ca461f905cb44f70b9df12000cd02cbbf9333c56
jordy-manner <jordy.manner@milkcreation.fr>
2021-10-01 17:33:03 +0200

    release/v1.0.6 Add database implementation

--- src/App.php
+++ src/App.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App;
 
 use App\Providers\AssetServiceProvider as AppAssetServiceProvider;
+use App\Providers\DatabaseServiceProvider as AppDatabaseServiceProvider;
 use App\Providers\DebugServiceProvider as AppDebugServiceProvider;
 use App\Providers\RoutingServiceProvider as AppRoutingServiceProvider;
 use App\Providers\ViewServiceProvider as AppViewServiceProvider;
 use Pollen\Asset\AssetServiceProvider;
+use Pollen\Database\DatabaseServiceProvider;
 use Pollen\Debug\DebugServiceProvider;
 use Pollen\Kernel\Application;
 use Pollen\Routing\RoutingServiceProvider;
@@ -22,12 +24,14 @@ class App extends Application
         return [
             // Components
             AssetServiceProvider::class,
+            DatabaseServiceProvider::class,
             DebugServiceProvider::class,
             RoutingServiceProvider::class,
             ViewServiceProvider::class,
             ViewExtendsServiceProvider::class,
             // Application
             AppAssetServiceProvider::class,
+            AppDatabaseServiceProvider::class,
             AppDebugServiceProvider::class,
             AppRoutingServiceProvider::class,
             AppViewServiceProvider::class
